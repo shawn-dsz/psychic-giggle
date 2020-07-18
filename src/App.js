a8e2ac5b810fa9f18550af0744733a62502b1c57
Shawn <shdsouza@seek.com.au>
2020-07-18 15:13:07 +1000

    Fix gh action

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React again
+            Learn React
           </a>
         </header>
       </div>
