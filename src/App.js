aa71223e6d717781dd441545fb3528cfe02c27fd
Shawn <shdsouza@seek.com.au>
2020-07-18 15:03:47 +1000

    update workflow2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React again
           </a>
         </header>
       </div>
